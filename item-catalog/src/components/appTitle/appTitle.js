6f92d413c536239c40cf8a88cab47dccbce5df14
donoftime2018 <donoftime2018@gmail.com>
2023-08-29 23:50:09 -0400

    bootstrap title on dashboard

--- item-catalog/src/components/appTitle/appTitle.js
+++ item-catalog/src/components/appTitle/appTitle.js
@@ -1,1 +1,14 @@
 import React from "react";
+import Badge from "react-bootstrap/Badge"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./appTitle.css"
+
+const Title = ({title}) => {
+    return(<>
+    <div class="titleBadge">   
+        <Badge pill bg="primary" style={{padding: "2% 5%", border: '3px solid black', textShadow: '3px 2px black'}}><h1>{title}</h1></Badge>
+    </div>
+    </>)
+}
+
+export default Title;
