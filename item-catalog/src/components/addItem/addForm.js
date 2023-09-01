fd36ab3d125e1382167debb553bed831f7186f3e
donoftime2018 <donoftime2018@gmail.com>
2023-09-01 19:14:22 -0400

    have formik for form validation.

--- item-catalog/src/components/addItem/addForm.js
+++ item-catalog/src/components/addItem/addForm.js
@@ -8,14 +8,14 @@ import axios from "axios";
 
 const AddForm = () => {
 
-    const [name, setName] = useState("")
+    const [name, setName] = useState('')
     const [price, setPrice] = useState(0)
-    const [desc, setDesc] = useState("")
+    const [desc, setDesc] = useState('')
     // const [quantity, setQuantity] = useState(null)
 
     const validation = yup.object({
         item_name: yup.string("Enter name of item").required("Item name required"),
-        item_price: yup.number().required("Item price required"),
+        item_price: yup.number().min(10.00).required("Item price required"),
         item_desc: yup.string("Enter description of item").required("Item description required")
     })
 
@@ -32,7 +32,7 @@ const AddForm = () => {
     }, {})
 
 
-    const addItemToDB = async(itemName, itemPrice, itemDesc) => {
+    const addItemToDB =(itemName, itemPrice, itemDesc) => {
         
         setName(itemName)
         setPrice(itemPrice)
@@ -65,18 +65,68 @@ const AddForm = () => {
             <Divider></Divider>
             <CardContent>
                 <form onSubmit={formik.handleSubmit}>
-                <Input sx={{border: '1px black solid', backgroundColor: 'white', borderRadius: '25px'}} placeholder="Item Name" disableUnderline="true" type="text"/>
+                <div>
+                    <TextField 
+                        id="item_name"
+                        name="item_name"
+                        variant="outlined"
+                        label="Item Name"
+                        value={formik.values.item_name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.item_name && Boolean(formik.errors.item_name)}
+                        helperText={formik.touched.item_name && formik.errors.item_name}
+                        sx={{ backgroundColor: 'white', /*borderRadius: '25px'*/}} 
+                        placeholder="Item name goes here..." 
+                        disableUnderline="true" 
+                    />
+                </div>
             {/* <Divider></Divider> */}
-               <Input inputProps={{step: 0.01}} sx={{border: '1px black solid',  backgroundColor: 'white', width:'60px', borderRadius: '25px'}} placeholder="Item Price" disableUnderline="true" type="number"></Input>
+                <div>
+                    <TextField 
+                        id="item_price"
+                        name="item_price"
+                        variant="outlined"
+                        value={formik.values.item_price}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.item_price && Boolean(formik.errors.item_price)}
+                        helperText={formik.touched.item_price && formik.errors.item_price}
+                        inputProps={{step: 0.01}} 
+                        sx={{backgroundColor: 'white'/*borderRadius: '25px'*/}} 
+                        placeholder="Item Price goes here..." 
+                        label="Item Price"
+                        disableUnderline="true" 
+                        type="number"
+                    />
+                </div>
             {/* <Divider></Divider> */}
-                <Input sx={{overflowX: 'auto', backgroundColor: 'white', border: '1px black solid', borderRadius: '25px'}} disableUnderline="true" type="text"></Input>
+                <div>
+                    <TextField
+                        id="item_desc"
+                        name="item_desc"
+                        variant="outlined"
+                        value={formik.values.item_desc}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.item_desc && Boolean(formik.errors.item_desc)}
+                        helperText={formik.touched.item_desc && formik.errors.item_desc}
+                        sx={{ backgroundColor: 'white', /*borderRadius: '25px'*/}} 
+                        placeholder="Item Desc goes here..." 
+                        label="Item Description"
+                        disableUnderline="true" 
+                        type="text"
+                    />
+                </div>
             {/* <Divider></Divider> */}
             {/* <CardContent>Item Quantity: <Input sx={{overflowX: 'auto', width: '70px', backgroundColor: 'white', border: '1px black solid', borderRadius: '25px'}} disableUnderline="true" type="text" onChange={(event)=>setQuantity(event.currentTarget.value)}></Input></CardContent>
             <Divider></Divider> */}
             {/* <CardContent>Item Rating (between 0 and 10): <Input sx={{overflowX: 'auto', width: '40px', backgroundColor: 'white', border: '1px black solid', borderRadius: '25px'}} disableUnderline="true" type="text" onChange={(event)=>setRating(event.currentTarget.value)}></Input></CardContent>
             <Divider></Divider> */}
-                <Button variant="contained" color="primary" sx={{borderRadius: '25px', border: '1px solid black'}}>Add Item to List</Button>
-                </form>
+                <div>
+                    <Button type="Submit" variant="contained" color="primary" sx={{borderRadius: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Add Item to List</Button>
+                </div>
+            </form>
                 {/* <Input sx={{border: '1px black solid', backgroundColor: 'white', borderRadius: '25px'}} placeholder="Item Name" disableUnderline="true" type="text"/>
             {/* <Divider></Divider> */}
                {/* <Input inputProps={{step: 0.01}} sx={{border: '1px black solid',  backgroundColor: 'white', width:'60px', borderRadius: '25px'}} placeholder="Item Price" disableUnderline="true" type="number"></Input> */}
