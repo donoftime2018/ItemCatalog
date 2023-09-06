be937546c549800ab5633fe10630900b0ee38683
donoftime2018 <donoftime2018@gmail.com>
2023-09-06 12:35:35 -0400

    removing desc

--- item-catalog/src/components/appTitle/appTitle.js
+++ item-catalog/src/components/appTitle/appTitle.js
@@ -4,13 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./appTitle.css"
 import { Component } from "react";
 
-const Title = ({title, titleDesc}) => {
+const Title = ({title}) => {
     return(<>
     <div class="titleBadge">   
         <Badge pill bg="primary" style={{padding: "2% 5%", border: '3px solid black', textShadow: '3px 2px black'}}>
             <div>
                 <h1 style={{textDecoration: 'underline'}}>{title}</h1>
-                <p>{titleDesc}</p>
+                {/* <p>{titleDesc}</p> */}
             </div>
         </Badge>
 
