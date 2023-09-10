c6e85a477fe85e4bf19d5d5496da3486dbf24e2a
donoftime2018 <donoftime2018@gmail.com>
2023-09-10 17:29:35 -0400

    removing unnecessary

--- item-catalog/src/index.tsx
+++ item-catalog/src/index.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import {createRoot, Root} from "react-dom/client"
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
     // <React.StrictMode>
         <BrowserRouter>
-            <App/>
+                <App/>
         </BrowserRouter>
     // </React.StrictMode>
 )
