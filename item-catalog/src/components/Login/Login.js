e08f7ada7938b8f7dd8de7c388ea36174c6d3a5e
donoftime2018 <donoftime2018@gmail.com>
2023-10-04 23:06:21 -0400

    Login page

---
+++ item-catalog/src/components/Login/Login.js
@@ -0,0 +1,86 @@
+import {React, useState} from "react";
+import {Card, CardContent, Divider, TextField, Button, CardHeader} from "@mui/material"
+import {useFormik} from "formik";
+import * as yup from "yup"
+import "./Login.css";
+
+const LoginPage = () => {
+    
+    const validation = () => yup.object({
+        userName: yup.string().min(10, "Username must be at least 10 characters long").max(20, "Username cannot be more than 20 characters").required("Username required"),
+        passWord: yup.string().min(8, "Password must be at least 8 characters long").max(12, "Password cannot be over 12 characters long").required("Password required")
+    })
+
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues: {
+           userName: "",
+           passWord: ""
+        },
+        validationSchema: validation,
+        onSubmit: (values, actions)=>{
+            // addItemToDB(values.item_name, values.item_price, values.item_desc);
+        }
+    }, {})
+
+    return(<>
+        <div class="loginLayout">
+        <Card class="loginCard">
+            <CardHeader sx={{textAlign: 'center'}} title="Login"></CardHeader>
+            <Divider/>
+            <CardContent>
+                <form onSubmit={formik.handleSubmit}>
+                    <div style={{display: "flex", justifyContent: 'center'}}>
+                        <TextField
+                            id="userName"
+                            name="userName"
+                            variant="outlined"
+                            type="text"
+                            label="User Name"
+                            value={formik.values.userName}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.userName && Boolean(formik.errors.userName)}
+                            helperText={formik.touched.userName && formik.errors.userName}
+                            sx={{ backgroundColor: 'white', /*borderRadius: '25px'*/}} 
+                            placeholder="User name goes here..." 
+                            disableUnderline="true" 
+                        ></TextField>
+                    </div>
+
+                    <div style={{display: "flex", justifyContent: 'center'}}>
+                        <TextField
+                            id="passWord"
+                            name="passWord"
+                            variant="outlined"
+                            type="password"
+                            label="Password"
+                            value={formik.values.passWord}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.passWord && Boolean(formik.errors.passWord)}
+                            helperText={formik.touched.passWord && formik.errors.passWord}
+                            sx={{ backgroundColor: 'white', /*borderRadius: '25px'*/}} 
+                            placeholder="Password goes here..." 
+                            disableUnderline="true" 
+                        ></TextField>
+                    </div>
+
+                    <div style={{display: "flex", justifyContent: 'center'}}>
+                        <Button type="Submit" variant="contained" color="primary" sx={{borderRadius: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Submit Info</Button>
+                    </div>
+
+            </form>
+            </CardContent>
+            <Divider></Divider>
+            <CardContent style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center'}}>
+                <p>Forgot Password? Click here.</p>
+                <p>Register? Click here</p>
+            </CardContent>
+        </Card>
+        </div>
+        
+    </>)
+}
+
+export default LoginPage;
