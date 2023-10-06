73baf6effe383712ab72e586b78d9f5cd904f27f
donoftime2018 <donoftime2018@gmail.com>
2023-10-06 18:41:59 -0400

    user doesn't log out when refresh anymore

--- item-catalog/src/components/context/user.js
+++ item-catalog/src/components/context/user.js
@@ -4,18 +4,29 @@ import { useNavigate } from 'react-router-dom';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+  
+    const [user, setUser] = useState(sessionStorage.getItem("signedInUser"));
     const navigate = useNavigate()
 
     const login = (user) => {
+        sessionStorage.setItem('signedInUser', user)
         setUser(user)
     }
 
     const logout = () => {
         setUser(null)
+        sessionStorage.setItem('signedInUser', null)
         navigate("/login")
         
     }
+
+    useEffect(()=>{
+        if(sessionStorage.getItem('signedInUser')!==null)
+        {
+            setUser(sessionStorage.getItem('signedInUser'))
+        }
+
+    }, [user])
 
     return (<>
         <AuthContext.Provider value={{user, login, logout}}>
