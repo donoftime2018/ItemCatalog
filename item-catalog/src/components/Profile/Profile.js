76df168d96a98af8d9310d051331e62406bdac8f
donoftime2018 <donoftime2018@gmail.com>
2023-10-09 16:23:39 -0400

    made profile component

---
+++ item-catalog/src/components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import {useEffect, useState} from "react";
+import {Card, CardContent, Divider} from "@mui/material"
+import axios from "axios";
+import "./Profile.css";
+
+const Profile = () => {
+    return(<>
+    </>)
+}
+
+export default Profile;
