5bad162dfbd576f7319566152bc3ec814139da54
donoftime2018 <donoftime2018@gmail.com>
2023-10-09 17:53:17 -0400

    name in navbar links to Profile

--- item-catalog/src/components/NavBar/NavBar.js
+++ item-catalog/src/components/NavBar/NavBar.js
@@ -1,6 +1,8 @@
 import {React} from "react";
 import { useAuth } from "../context/user";
-import { AppBar, Typography, Box, Button } from "@mui/material";
+import { AppBar, Typography, IconButton, Box, Button } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from "react-router-dom";
 
 const AppNav = () => {
     const auth = useAuth();
@@ -12,11 +14,12 @@ const AppNav = () => {
     return(<>
         <AppBar sx={{paddingBottom: '10px', paddingTop:'0.4%', display: 'flex', flexDirection: 'row', alignItems: 'center'}}  position="static">
             <Box sx={{marginLeft: '5%', fontSize: '20px'}}>
-                Welcome, <span style={{fontWeight: 'bold'}}>{auth.user}</span>
+                Welcome, <Link style={{fontWeight: 'bold', color: 'white'}} to="/profile">{auth.user}</Link>
             </Box>
             <Box sx={{flexGrow: '1'}}/>
             <Box sx={{marginRight: '5%', display: {xs: 'none', md: 'flex'}}}>
-                <Button variant="contained" color="error" sx={{border: '1px solid black', borderRadius: '25px'}} onClick={signOut}>Logout</Button>
+                {/* <IconButton sx={{justifyContent: 'space-between'}}><AccountCircleIcon color="success" fontSize='large'></AccountCircleIcon></IconButton> */}
+                <Button variant="contained" color="error" sx={{border: '1px solid black', borderRadius: '25px'}} onClick={signOut}>Log Out</Button>
             </Box>
         </AppBar>
     </>)
--- item-catalog/src/components/Profile/Profile.js
+++ item-catalog/src/components/Profile/Profile.js
@@ -1,9 +1,12 @@
 import {useEffect, useState} from "react";
 import {Card, CardContent, Divider} from "@mui/material"
 import axios from "axios";
+import { useAuth } from "../context/user";
 import "./Profile.css";
 
 const Profile = () => {
+    const auth = useAuth()
+    
     return(<>
     </>)
 }
