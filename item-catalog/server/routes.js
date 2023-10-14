2dc06400d2534c33ffed9d20ab68c43294c6c3e7
donoftime2018 <donoftime2018@gmail.com>
2023-10-13 20:18:47 -0400

    limiting and arranging Items liked and posted

--- item-catalog/server/routes.js
+++ item-catalog/server/routes.js
@@ -26,7 +26,7 @@ router.route('/').get(async(req, res)=>{
 router.route("/getPostedItems").post(async(req, res)=>{
     let user = req.body.user;
 
-    Item.find({poster: user}).sort({name: 1}).then(function (data) {
+    Item.find({poster: user}).sort({updatedAt: -1}).limit(5).then(function (data) {
         res.json(data).status(200).send()
         
     }).catch(function(error) {console.error(error)})
@@ -35,7 +35,7 @@ router.route("/getPostedItems").post(async(req, res)=>{
 
 router.route("/getLikedItems").post((req, res) => {
     let user = req.body.user
-    Item.find({usersRated: user}).sort({name: 1}).then(function (data) {
+    Item.find({usersRated: user}).sort({updatedAt: -1}).limit(5).then(function (data) {
         // console.log(req.body)
         // console.log(data)
          res.json(data).status(200).send()
@@ -46,7 +46,7 @@ router.route("/getLikedItems").post((req, res) => {
 router.route("/getRecommendedItems").post((req, res) => {
     let user = req.body.user
 
-    Item.find({$and: [{poster: {$ne: user}}, {usersRated: {$nin: user}}]}).sort({rating: -1, name: 1}).limit(4).then(function (data) {
+    Item.find({$and: [{poster: {$ne: user}}, {usersRated: {$nin: user}}]}).sort({rating: -1, name: 1}).limit(5).then(function (data) {
         res.json(data).status(200).send()
     }).catch(function(error) {console.error(error)})
 })
