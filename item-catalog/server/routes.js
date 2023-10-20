063beb2412ddbe600cb12079f7743217f260b6aa
donoftime2018 <donoftime2018@gmail.com>
2023-10-20 16:22:22 -0400

    most popular items listed on profile

--- item-catalog/server/routes.js
+++ item-catalog/server/routes.js
@@ -33,6 +33,14 @@ router.route("/getPostedItems").post(async(req, res)=>{
 
 })
 
+router.route("/mostPopularItems").post(async(req, res)=>{
+    let user = req.body.user;
+
+    Item.find({poster: user}).sort({rating: -1}).limit(5).then(function(data) {
+        res.json(data).status(200).send()
+    }).catch(function(error) {console.error(error)})
+})
+
 router.route("/getLikedItems").post((req, res) => {
     let user = req.body.user
     Item.find({usersRated: user}).sort({updatedAt: -1}).limit(5).then(function (data) {
