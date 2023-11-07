c3c15a57e12f16378af6f439bd83a0cb4a844034
donoftime2018 <donoftime2018@gmail.com>
2023-11-06 21:05:53 -0500

    environment variables added

--- item-catalog/server/server.js
+++ item-catalog/server/server.js
@@ -5,16 +5,16 @@ const bodyParser = require('body-parser');
 const itemRoute = require("./routes.js")
 const userRoute = require("./authRoutes.js")
 const app = express()
+require('dotenv').config()
 
-
-mongoose.connect('mongodb://127.0.0.1/items').then(()=>{console.log("Connected!")}).catch((err)=>{console.error(err)})
+mongoose.connect(process.env.MONGOURL).then(()=>{console.log("Connected!")}).catch((err)=>{console.error(err)})
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 app.use("/items", itemRoute)
 app.use(userRoute)
 
-const port = 4000
+const port = process.env.PORT | 4000
 
 app.listen(port, ()=>{
     console.log("Connected to port " + port)
