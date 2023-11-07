4a4046cd563e43c7968982e3748516478abbdd45
donoftime2018 <donoftime2018@gmail.com>
2023-11-07 17:47:58 -0500

    Settings on navbar works

--- item-catalog/src/components/NavBar/Settings.js
+++ item-catalog/src/components/NavBar/Settings.js
@@ -7,17 +7,42 @@ import { Tooltip, IconButton} from "@mui/material";
 
 
 const Settings = () => {
-    return (<>
-        <Tooltip title="Settings"><IconButton><SettingsIcon fontSize="large"></SettingsIcon></IconButton></Tooltip>
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
 
-        <Menu>
+    return (<>
+        <Tooltip title="Settings">
+            <IconButton 
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                color="inherit">
+                <SettingsIcon fontSize="large"></SettingsIcon>
+            </IconButton>
+            </Tooltip>
+
+        <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+            'aria-labelledby': 'basic-button',
+            }}
+        >
             <MenuItem sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <DeleteForever fontSize="large" />
-                Delete Profile
+                <Tooltip title="Delete Profile"><DeleteForever fontSize="large" /></Tooltip>
             </MenuItem>
             <MenuItem sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Edit fontSize="large" />
-                Edit Items
+                {/* <Tooltip title="Edit Items"><Edit fontSize="large" /></Tooltip> */}
             </MenuItem>
         </Menu>
     </>)
