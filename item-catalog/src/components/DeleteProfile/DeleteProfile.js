ecd266e1884668eadba388d8b01e2f5f2ca9a1b8
donoftime2018 <donoftime2018@gmail.com>
2023-11-07 21:21:25 -0500

    settings points to correct path

--- item-catalog/src/components/DeleteProfile/DeleteProfile.js
+++ item-catalog/src/components/DeleteProfile/DeleteProfile.js
@@ -1,10 +1,26 @@
 import {React, useState} from "react"
 import axios from "axios"
-import {Card, Content, Divider, Button} from "@mui/material"
-
+import {Card, CardHeader, CardContent, TextField, Divider, Button} from "@mui/material"
+import { useFormik } from "formik"
+import "./DeleteProfile.css"
 
 const DeleteProfile = () => {
-    return(<></>)
+
+
+
+    return(<>
+        <div class="deleteLayout">
+            <Card class="deleteCard">
+                <CardHeader  sx={{textAlign: 'center'}} title="Delete Profile"></CardHeader>
+                <Divider></Divider>
+                <CardContent>
+                    <form>
+                        
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    </>)
 }
 
 export default DeleteProfile
