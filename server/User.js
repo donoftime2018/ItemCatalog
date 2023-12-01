73c86c7074c54848d533269dcf7b0f409bc04c2e
donoftime2018 <donoftime2018@gmail.com>
2023-12-01 12:56:19 -0500

    removed ItemsInCart from user schema

--- server/User.js
+++ server/User.js
@@ -14,35 +14,11 @@ const userSchema = new mongoose.Schema({
     email:  {
         type: String,
         required: true
-    },
-
-    itemsInCart: {
-        type: [{
-            name: {type: String},
-            price: {type: Number},
-            quantityAdded: {
-                type: Number,
-                default: 0
-            },
-            maximumQuantity: {type: Number}
-        }]
     }
 
 }, {
     collection: 'user',
     timestamps: true
 })
-
-// userSchema.pre('validate', function() {
-//     if (this.itemsInCart.length > 10 && this.itemsBought.length > 10)
-//     {
-//         this.itemsinCart.pop()
-//         this.itemsBought.pop()
-
-//         console.log(this.itemsinCart)
-//         console.log(this.itemsinBought)
-//     }
-// })
-
 
 module.exports = mongoose.model("User", userSchema);
