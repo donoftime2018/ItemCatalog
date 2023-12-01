f58a369432123ce21b7c4f44769a31e4f21d5197
donoftime2018 <donoftime2018@gmail.com>
2023-11-30 22:27:20 -0500

    itemsInCart and itemsBought in User schema

--- server/User.js
+++ server/User.js
@@ -14,10 +14,38 @@ const userSchema = new mongoose.Schema({
     email:  {
         type: String,
         required: true
-    }
+    },
+
+    itemsInCart: [
+        {
+            itemName: String,
+            addedBy: String,
+            itemPrice: Number
+        }
+    ],
+
+    itemsBought : [
+        {
+            itemName: String,
+            addedBy: String,
+            itemPrice: Number
+        }
+    ]
 }, {
     collection: 'user',
     timestamps: true
 })
+
+userSchema.pre('validate', function() {
+    if (this.itemsInCart.length > 10 && this.itemsBought.length > 10)
+    {
+        this.itemsinCart.pop()
+        this.itemsBought.pop()
+
+        console.log(this.itemsinCart)
+        console.log(this.itemsinBought)
+    }
+})
+
 
 module.exports = mongoose.model("User", userSchema);
--- server/purchaseRoutes.js
+++ server/purchaseRoutes.js
@@ -1,4 +1,18 @@
 const mongoose = require('mongoose')
 const express = require('express');
 const router = express.Router()
-const Cart = require("./Cart")
+const User = require('./User');
+
+router.route("/addToCart/:user").put(async(req, res) => {
+    
+})
+
+router.route("/numItemsInCart/:user").get(async(req, res) => {
+    
+})
+
+router.route("/itemsInCart/:user").get(async(req, res) => {
+
+})
+
+module.exports = router
