db09bb30a8bb80949621fd3c8b1cc2c5da87185c
donoftime2018 <donoftime2018@gmail.com>
2023-11-30 21:22:34 -0500

    pill for num items and purchase title

--- item-catalog/src/components/Buying/Buying.js
+++ item-catalog/src/components/Buying/Buying.js
@@ -1,14 +1,20 @@
 import {React, useState, useEffect} from "react";
 import {Card, CardContent, Divider, CardHeader} from "@mui/material"
 import AppNav from "../NavBar/NavBar";
+import Title from "../appTitle/appTitle";
+import { useAuth } from "../context/user";
 import "./Buying.css"
 
 const Buying = () => {
+
+    const auth = useAuth()
+    const user = auth.user
+
     return(<>
         <AppNav></AppNav>
+        <Title title={user+"'s Cart"}></Title>
         <div class="layout">
             <Card class="inCartItems">
-                <CardHeader sx={{textAlign: 'center', textDecoration: 'underline'}} title="Your Cart:"></CardHeader>
             </Card>
         </div>
     </>)
--- item-catalog/src/components/NavBar/NavBar.js
+++ item-catalog/src/components/NavBar/NavBar.js
@@ -6,6 +6,8 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from "react-router-dom";
 import Settings from "./Settings";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Badge from "react-bootstrap/Badge"
 
 const AppNav = () => {
     const auth = useAuth();
@@ -37,6 +39,7 @@ const AppNav = () => {
             <Box sx={{ marginRight: '5%', display: {xs: 'none', md: 'flex', alignItems: 'center'}, alignItems: 'center'}}>
                 <Button variant="contained" color="warning" sx={{border: '1px solid black', color: 'black', borderRadius: '25px'}} onClick={signOut}>Log Out</Button>
                 <Tooltip title="Purchase Items"><IconButton onClick={purchaseItems} color="inherit"><ShoppingCartIcon fontSize="large"></ShoppingCartIcon></IconButton></Tooltip>
+                <Badge pill bg="danger" style={{fontFamily: 'Arial', marginLeft: '-2%', fontSize: 'large'}}>9</Badge>
             </Box>
         </AppBar>
        </>)
