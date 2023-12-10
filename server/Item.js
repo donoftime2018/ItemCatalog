b105cdc175391a67f93025a1a00789ced0d9e806
donoftime2018 <donoftime2018@gmail.com>
2023-12-10 16:14:39 -0500

    Schema declared differently

--- server/Item.js
+++ server/Item.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const {Schema} = mongoose
 
-const itemSchema = new mongoose.Schema({
+const itemSchema = new Schema({
     name: {
         type: String,
         required: true
