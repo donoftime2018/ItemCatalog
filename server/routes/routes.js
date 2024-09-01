38982a3722bd3da1f30a6768c386a1ec925780bd
donoftime2018 <donoftime2018@gmail.com>
2024-09-01 19:17:53 -0400

    removing unnecessary comments

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,14 +5,7 @@ const Item = require('../schemas/Item.js');
 
 mongoose.set('setDefaultsOnInsert', true);
 
-
-//Read all items
 app.get("/", async(req, res)=>{
-    // Item.find({}).sort({rating: -1, price: 1, name: 1}).then(function(data) {
-    //         res.json(data).status(200).send()
-    // }).catch(function(err){
-        
-    // })
 
     try {
         let allItems = await Item.find({}).sort({ rating: -1, price: 1, name: 1})
@@ -28,16 +21,7 @@ app.get("/getPostedItems/:user", async(req, res)=>{
     try {
         let itemsPosted = await Item.find({poster: user}).select("name").sort({updatedAt: -1}).limit(5)
         res.status(200).json(itemsPosted)
-    } catch(err)
-    {
-
-    }
-
-    // Item.find({poster: user}).sort({updatedAt: -1}).limit(5).then(function (data) {
-    //     res.json(data).status(200).send()
-        
-    // }).catch(function(error) {})
-
+    } catch(err){}
 })
 
 app.get("/numPostedItems/:user", async(req, res)=>{
@@ -47,7 +31,6 @@ app.get("/numPostedItems/:user", async(req, res)=>{
         const count = await Item.countDocuments({poster: user});
         res.status(200).json(count)
     } catch (error) {
-        // console.error(error)
     }
 
 })
@@ -59,17 +42,12 @@ app.get("/numLikedItems/:user", async(req, res)=>{
         const count = await Item.countDocuments({usersRated: user});
         res.status(200).json(count)
     } catch(error){
-        // console.error(error)
     }
 })
 
 app.get("/mostPopularItems/:user", async(req, res)=>{
-    let user = req.params.user;
 
-    // Item.find({poster: user}).sort({rating: -1, updatedAt: -1}).limit(5).then(function(data) {
-    //     res.json(data).status(200).send()
-    // }).catch(function(error) {console.error(error)}
-    
+    let user = req.params.user;
     try {
         let popularItems = await Item.find({poster: user, rating: {$gte: 1}}).select("name rating").sort({rating: -1, updatedAt: -1}).limit(5)
         res.status(200).json(popularItems)
@@ -80,11 +58,7 @@ app.get("/mostPopularItems/:user", async(req, res)=>{
 
 app.get("/getLikedItems/:user", async(req, res) => {
 
-
     let user = req.params.user
-    // Item.find({usersRated: user}).sort({updatedAt: -1}).limit(5).then(function (data) {
-    //      res.json(data).status(200).send()
-    // }).catch(function(error) {console.error(error)})
     try {
         let likedItems = await Item.find({usersRated: user}).select("name").sort({updatedAt: -1}).limit(5)
         res.status(200).json(likedItems)
@@ -93,49 +67,16 @@ app.get("/getLikedItems/:user", async(req, res) => {
     }
 })
 
-//add items 
+
 app.post("/insertItems", async(req, res)=>{
 
-    // Item.find({$or: [
-    //     {name: req.body.name, desc: req.body.desc, price: req.body.price},
-    //     {name: req.body.name, desc: req.body.desc},
-    //     {name: req.body.name, price: req.body.price},
-    //     {name: req.body.name}
-    // ]
-    // }).then(function(data){
-    //     console.log(req.body)
-    //     console.log(data)
-    //     if(data.length > 0)
-    //     {
-    //         console.log("Item exists");
-    //         res.status(400).send({msg: req.body.name + " already exists"});
-            
-    //     }
-    //     else {
-    //         Item.create({name: req.body.name, desc: req.body.desc, price: req.body.price, poster: req.body.user}).then(
-    //             (result)=>{
-    //             console.log(result); 
-    //             res.status(200).send()}).catch((err)=>{console.error(err)})
-    //     }
-    // }).catch(function(err){
-    //     // console.error(err)
-    // })
-
     try {
-    
-        // if (findItem.length>0)
-        // {
-        //     res.status(400).send({msg: req.body.name + " already exists"});
-        // }
-    
-        // else {
         let newItem = await Item.create({name: req.body.name, desc: req.body.desc, price: req.body.price, poster: req.body.user})
         console.log(newItem)
         if(newItem)
         {
             res.status(200).send()
         }
-        // }
     } catch(err)
     {
         console.log(err);
@@ -145,14 +86,12 @@ app.post("/insertItems", async(req, res)=>{
     
 })
 
-//delete items
 app.delete("/deleteItems/:id", async(req, res)=>{
    Item.deleteOne({_id: req.params.id}).then((result)=>{console.log(result); res.status(200).send()}).catch((err)=>{
-        // console.error(err)
     })
 })
 
-//update rating
+
 app.put("/increaseRating/:id", async(req, res, next)=>{
 
     Item.findOne({_id: req.params.id}).then((doc)=>{
@@ -215,7 +154,5 @@ app.put("/decreaseRating/:id", async(req, res, next)=>{
             res.status(400).send({msg: err})
     })
 })
-
-
 
 module.exports = app
