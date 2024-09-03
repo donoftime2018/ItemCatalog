04cb84d789962852d88403080d472086640b10aa
donoftime2018 <donoftime2018@gmail.com>
2024-09-03 15:28:01 -0400

    bold title on modal

--- item-catalog/src/components/appTitle/appTitle.js
+++ item-catalog/src/components/appTitle/appTitle.js
@@ -55,7 +55,7 @@ const Title = ({title}) => {
 
     <Modal open={open} onClose={handleClose}>
         <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h5" style={{margin: '5px 0px'}}>Put a Price On It!</Typography>
+            <Typography id="modal-modal-title" variant="h5" style={{margin: '5px 0px', fontWeight: 'bold'}}>Put a Price On It!</Typography>
             <Divider></Divider>
             <Typography id="modal-modal-description" variant="p" style={{margin: '5px 0px', lineHeight: '1.25'}}>Put a Price On It! is a  MERN Stack application where users can add information -- name, price, and a short description -- about items they find on eBay, Amazon, etc. Other users can see the items' price, description and popularity amongst users of the app to hopefully aid in making informed decisions about which items to buy. Users can post items and like items that they have not posted. Users can only like an item once, however (i.e. a user cannot like an item 2+ times). The most popular items with the lowest price are the first items users will see upon logging in. Users are also granted a profile showing recent activity on the app such as most popular items, recently liked and posted items, and total number of liked and posted items.</Typography>
         </Box>
