9d01f7c309158300761ea79e745e315b410dd462
donoftime2018 <donoftime2018@gmail.com>
2024-09-03 21:46:32 -0400

    alert fades in and out

--- item-catalog/src/components/Alert/Alert.js
+++ item-catalog/src/components/Alert/Alert.js
@@ -6,7 +6,7 @@ const AppAlert = ({message}) => {
  
     return(<>
         <div class="itemAlert">
-            <Alert sx={{width: '25%', display: 'flex', alignItems: 'center', justifyContent: 'center'}} variant="filled" severity="success">
+            <Alert sx={{width: '25%', borderRadius: '25px', display: 'flex', alignItems: 'center', justifyContent: 'center'}} variant="filled" severity="success">
                 <AlertTitle sx={{display: 'flex', alignContent: 'center', justifyContent: 'center'}}>{message}</AlertTitle>
             </Alert>
         </div>
