9fd4d32f915f88d517d71f0a8a3d78fc9b94c543
donoftime2018 <donoftime2018@gmail.com>
2024-12-03 15:51:55 -0500

    remove import dotenv from index.tsx

--- item-catalog/src/index.tsx
+++ item-catalog/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import {createRoot} from "react-dom/client"
 import App from "./App";
-import 'dotenv/config'
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
