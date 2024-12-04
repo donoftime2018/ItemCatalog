836691f5338970e7e3e5cadeb30402ad33604690
donoftime2018 <donoftime2018@gmail.com>
2024-12-04 00:19:08 -0500

    using correct server .env variable for API endpoint in Add Forms component

--- item-catalog/src/components/addItem/addForm.js
+++ item-catalog/src/components/addItem/addForm.js
@@ -62,7 +62,7 @@ const AddForm = () => {
         const user = auth.user
         const data = {name, price, desc, user}
 
-        axios.post(process.env.SERVER_URL + "/items/insertItems", data).then((res)=>{
+        axios.post(process.env.REACT_APP_SERVER_URL + "/items/insertItems", data).then((res)=>{
             if (res.status === 200)
             {
                 setAlertOpen(true)
