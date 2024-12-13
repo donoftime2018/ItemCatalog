d7f37e68632463668203cd68c2be33bf0e6dc8c1
donoftime2018 <donoftime2018@gmail.com>
2024-12-13 17:12:23 -0500

    scrollbar doesn't disappear when opening Modals for Item and app description

--- item-catalog/src/components/Item/Item.js
+++ item-catalog/src/components/Item/Item.js
@@ -166,7 +166,8 @@ const Item = ({itemName, itemDesc, itemPoster, itemRatedByUser, itemPrice, itemR
          </Card>
 
          <Modal open={open} 
-            onClose={closeDesc}
+            onClose={closeDesc}            
+            disableScrollLock={true}
             >
                 <Box sx={style}>
                     <Typography id="modal-modal-title" variant="h5" style={{margin: '5px 0px', lineHeight: '1.25'}}>{itemName}</Typography>
