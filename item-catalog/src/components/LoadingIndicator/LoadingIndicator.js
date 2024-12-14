174322b4bf7df8170e4ac6cbc7052f589a6b0b4c
donoftime2018 <donoftime2018@gmail.com>
2024-12-13 20:28:02 -0500

    LoadingIndicator component created

---
+++ item-catalog/src/components/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {OrbitProgress} from "react-loading-indicators"
+
+const LoadingIndicator = () => {
+    return (<>
+        <OrbitProgress color="#32cd32" size="medium" text="" textColor="" />
+    </>)
+}
+
+export default LoadingIndicator;
