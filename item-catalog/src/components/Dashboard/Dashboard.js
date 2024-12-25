fd6273732c61987dfef2f771985461c55ec9e035
donoftime2018 <donoftime2018@gmail.com>
2024-12-24 21:06:39 -0500

    updated props for Item component include website item was found on

--- item-catalog/src/components/Dashboard/Dashboard.js
+++ item-catalog/src/components/Dashboard/Dashboard.js
@@ -151,7 +151,7 @@ const Dashboard = (props) => {
                 {
                     items.filter(item=>flexibleRegex.test(item.name) || flexibleRegex.test(item.desc)).map((item, index)=>{
                         return(<>
-                            <Item itemName={item.name} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
+                            <Item itemName={item.name} itemWebsite={item.website} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
                         </>)
                     })
                 }
@@ -168,7 +168,7 @@ const Dashboard = (props) => {
                 {
                     items.filter(item=>new RegExp(posterQuery).test(item.poster)).map((item, index)=>{
                         return(<>
-                            <Item itemName={item.name} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
+                            <Item itemName={item.name} itemWebsite={item.website} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
                         </>)
                     })
                 }
@@ -185,7 +185,7 @@ const Dashboard = (props) => {
                 {
                     items.filter(item=>new RegExp(posterQuery).test(item.poster) && (flexibleRegex.test(item.name) || flexibleRegex.test(item.desc))).map((item, index)=>{
                         return(<>
-                            <Item itemName={item.name} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
+                            <Item itemName={item.name} itemWebsite={item.website} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
                         </>)
                     })
                 }
@@ -203,7 +203,7 @@ const Dashboard = (props) => {
                     items.map((item, index)=>{
                  
                         return(<>
-                            <Item itemName={item.name} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
+                            <Item itemName={item.name} itemWebsite={item.website} itemDesc={item.desc} itemPoster={item.poster} itemRatedByUser={item.usersRated.includes(user)} itemPrice={item.price} itemRating={item.rating} dateCreated={item.createdAt} lastUpdated={item.updatedAt} id={index} dbID={item._id}></Item>
                         </>)
                     })
                 }
