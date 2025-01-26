43c1e466e3bf1aa6728f757aa2e31af991ee3d80
donoftime2018 <donoftime2018@gmail.com>
2025-01-26 01:07:17 -0500

    checkbox group and submit button for reporting item

--- item-catalog/src/components/reportItem/reportItem.js
+++ item-catalog/src/components/reportItem/reportItem.js
@@ -1,7 +1,7 @@
 import FlagIcon from '@mui/icons-material/Flag';
 import Backdrop from "@mui/material/Backdrop";
 import { useState } from "react";
-import {Tooltip, Card, CardHeader, CardContent, Divider} from "@mui/material";
+import {Tooltip, Card, CardHeader, CardContent, Button, FormGroup, FormControlLabel, Checkbox, Divider} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import * as yup from "yup"
 import axios from "axios";
@@ -34,6 +34,22 @@ const ReportItem = () => {
                 <Card class="reportFormStyle">
                     <CardHeader sx={{textAlign: 'center'}} title="Report Item"></CardHeader>
                     <Divider></Divider>
+                    <CardContent>
+                    <form>
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <FormGroup>
+                                <FormControlLabel control={<Checkbox/>} label="Sexual or Inappropriate Content"></FormControlLabel>
+                                <Divider></Divider>
+                                <FormControlLabel control={<Checkbox/>} label="Potentially Dangerous Content"></FormControlLabel>
+                                <Divider></Divider>
+                            </FormGroup>
+                        </div>
+
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <Button type="Submit" variant="contained" color="error" sx={{borderRadius: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Submit Report</Button>
+                        </div>
+                    </form> 
+                    </CardContent>
                 </Card>
             </div>
         </Backdrop>
