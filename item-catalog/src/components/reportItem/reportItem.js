9aaf1aa8bb1c48388b4148ad89cebb0ae38f2876
donoftime2018 <donoftime2018@gmail.com>
2025-01-26 01:19:00 -0500

    touched up UI for reporting item form

--- item-catalog/src/components/reportItem/reportItem.js
+++ item-catalog/src/components/reportItem/reportItem.js
@@ -34,18 +34,14 @@ const ReportItem = () => {
                 <Card class="reportFormStyle">
                     <CardHeader sx={{textAlign: 'center'}} title="Report Item"></CardHeader>
                     <Divider></Divider>
-                    <CardContent>
+                    <CardContent style={{display: 'flex', justifyContent: 'center', padding: '0px'}}>
                     <form>
-                        <div style={{display: 'flex', justifyContent: 'center'}}>
-                            <FormGroup>
-                                <FormControlLabel control={<Checkbox/>} label="Sexual or Inappropriate Content"></FormControlLabel>
-                                <Divider></Divider>
-                                <FormControlLabel control={<Checkbox/>} label="Potentially Dangerous Content"></FormControlLabel>
-                                <Divider></Divider>
-                            </FormGroup>
-                        </div>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox/>} label="Sexual or Inappropriate Content"></FormControlLabel>
+                            <FormControlLabel control={<Checkbox/>} label="Potentially Dangerous Content"></FormControlLabel>
+                        </FormGroup>
 
-                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <div style={{display: 'flex', justifyContent: 'center', padding: '10px'}}>
                             <Button type="Submit" variant="contained" color="error" sx={{borderRadius: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Submit Report</Button>
                         </div>
                     </form> 
