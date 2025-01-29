560558df27d48947fa58dde8808ae68ec57fa965
donoftime2018 <donoftime2018@gmail.com>
2025-01-29 17:05:27 -0500

    function for sending mail to user created

---
+++ server/routes/mailing.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer')
+
+const sendMail = (recipient, message, subject) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASS
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env.USER,
+        to: recipient,
+        subject: subject,
+        text: message
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
+
+export default sendMail
