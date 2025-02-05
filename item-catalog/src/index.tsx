1a656773dea08c47d24b439468f409af7f96562c
donoftime2018 <donoftime2018@gmail.com>
2025-02-05 12:32:57 -0500

    added googleoauthprovider to index.tsx and local_host/local_db_uri for signing in/registering

--- item-catalog/src/index.tsx
+++ item-catalog/src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import {createRoot} from "react-dom/client"
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="281759591846-n3rr2thcenv8amtpt863rou8e0sqotc5.apps.googleusercontent.com">
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </GoogleOAuthProvider>
+
 )
