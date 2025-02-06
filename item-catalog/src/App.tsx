0e29f34219966abbea40aff10379682a64c2b89d
donoftime2018 <donoftime2018@gmail.com>
2025-02-05 19:12:36 -0500

    protected route for User Profile route

--- item-catalog/src/App.tsx
+++ item-catalog/src/App.tsx
@@ -30,7 +30,11 @@ const App = () =>
                                 <Route element={<Register title="Sign Up"/>} path="/register"></Route>
                                 <Route element={<UpdatePassword title="Change Password"/>} path="/updatePassword"></Route>
                                 <Route element={<ForgotPassword title="Forgot Password"/>} path="/forgotPassword"></Route>
-                                <Route element={<Profile title="User Profile"/>} path="/profile">
+                                <Route element={
+                                    <ProtectedRoute>
+                                        <Profile title="User Profile"/>
+                                    </ProtectedRoute>
+                                } path="/profile">
                                 </Route>
                                 <Route element={<DeleteProfile title="Delete Account"/>} path="/deleteAccount"></Route>
                             </Routes>
