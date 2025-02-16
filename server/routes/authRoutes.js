76d4c8d90355bcf0e2024f879c24ccc358e721f4
donoftime2018 <donoftime2018@gmail.com>
2025-02-15 22:19:05 -0500

    using .env variable for url to reset password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -15,7 +15,6 @@ app.post("/login", async (req, res) => {
    
     try {
         let findUser = await User.findOne({$or: [{username: name}, {email: name}]})
-        console.log(findUser)
         if (findUser)
         {
             let matches = await bcrypt.compare(pwd, findUser.password)
@@ -70,7 +69,7 @@ app.post("/resetPasswordLink", async (req, res) => {
         else
         {
             const token = jwt.sign({userId: findUser._id}, process.env.JWT_SECRET, {expiresIn: '10m'})
-            sendMail(email, `<a href="http://localhost:3000/updatePassword/${token}">Click here to reset your password.</a> <p>The link expires in 10 minutes.</p>`, "Reset Password")
+            sendMail(email, `<a href="${process.env.LOCAL_CLIENT}/updatePassword/${token}">Click here to reset your password.</a> <p>The link expires in 10 minutes.</p>`, "Reset Password")
             res.status(200).send()
         }
     } catch(err) {
