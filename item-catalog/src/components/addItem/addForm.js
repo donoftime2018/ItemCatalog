f8dec639b54b49d7aaf4a9aff3ce57779656ab10
donoftime2018 <donoftime2018@gmail.com>
2025-02-23 15:03:26 -0500

    removed redundant display: flex and justifyContent: center stylings from addForm.js

--- item-catalog/src/components/addItem/addForm.js
+++ item-catalog/src/components/addItem/addForm.js
@@ -166,7 +166,7 @@ const AddForm = () => {
                         disableUnderline="true" 
                     />
                 </div>
-                <div >
+                <div style={{display: 'flex', justifyContent: 'center'}}>
                     <Button type="Submit" variant="contained" color="primary" sx={{borderRadius: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Add Item to Catalog</Button>
                 </div>
             </form>
