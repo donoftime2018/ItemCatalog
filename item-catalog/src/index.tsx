bf591aab3d0bc090f74c7fa3e117890d36abf1c6
donoftime2018 <donoftime2018@gmail.com>
2025-03-24 10:56:56 -0400

    created and using store for redux

--- item-catalog/src/index.tsx
+++ item-catalog/src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import {createRoot} from "react-dom/client"
+import {Provider} from "react-redux"
+import store from "./components/store/store";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+
 )
